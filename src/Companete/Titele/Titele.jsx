628833a46415b43bb5fb9072fdc7f9da309b66db
Your Name <you@example.com>
2024-06-03 00:04:15 +0300

    اسم التعhديل

--- src/Companete/Titele/Titele.jsx
+++ src/Companete/Titele/Titele.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Titele() {
   return (
-    <div className=' text-danger text-center'>
+    <div className=' text-danger bg-black text-center'>
       <h1>Hello In your to do list</h1>
     </div>
   );
--- src/Companete/To-do-list/ToDoList.jsx
+++ src/Companete/To-do-list/ToDoList.jsx
@@ -3,43 +3,73 @@ import React, { useRef, useState } from "react";
 export default function ToDoList() {
   const [task, setTask] = useState([]);
   const inputRef = useRef();
- 
+  let [forIndexNum, setForIndexNum] = useState(-1);
+
   const handerClickAddTodo = () => {
-    
-     const valueText = inputRef.current.value;
-    setTask([...task, valueText]);
-    inputRef.current.value = "";
+    if (inputRef.current.value) {
+      let nameBut = document.getElementById("butAddOrEdite");
+      if (nameBut.innerText == "Add") {
+        const valueText = inputRef.current.value;
+        setTask([...task, valueText]);
+        inputRef.current.value = "";
+      } else {
+        let edite = [...task];
+        edite.splice(forIndexNum, 1, inputRef.current.value);
+        setTask(edite);
+        nameBut.innerText = "Add";
+        inputRef.current.value = "";
+      }
+    }
   };
+
   const clickEdite = (index) => {
+    inputRef.current.value = task[index];
+    document.getElementById("butAddOrEdite").innerText = "Edite";
+    setForIndexNum(index);
 
     // task[index]=;
-    console.log("you clicked add Edite");
+    console.log(forIndexNum);
   };
   const clickDelete = (index) => {
-    const deleteFun = [...task]
-    deleteFun.splice(index, 1)
+    const deleteFun = [...task];
+    deleteFun.splice(index, 1);
     setTask(deleteFun);
     console.log("you clicked add Delete");
   };
 
   return (
     <>
-      <div className=" text-center  p-1 m-auto w-75">
+      <div className=" text-center bg-dark p-1 m-auto w-75">
         <input
           type="text"
           placeholder="enter your task ... ✍️"
           ref={inputRef}
         />
-        <button onClick={handerClickAddTodo}>add</button>
+        <button onClick={handerClickAddTodo} id="butAddOrEdite">
+          Add
+        </button>
       </div>
       <ul className=" list-unstyled  ">
         {task.map((iteam, index) => {
           return (
-            <li key={index} className=" d-flex border border-2 border-danger ">
+            <li
+              key={index}
+              className=" text-light d-flex border border-2 border-danger "
+            >
               {iteam}
               <div className=" d-inline but-end ">
-                <button onClick={() => clickEdite(index)}>Edite</button>
-                <button onClick={() => clickDelete(index)}>delete</button>
+                <button
+                  className=" btn bg-primary "
+                  onClick={() => clickEdite(index)}
+                >
+                  Edite
+                </button>
+                <button
+                  className=" btn bg-danger "
+                  onClick={() => clickDelete(index)}
+                >
+                  delete
+                </button>
               </div>
             </li>
           );
